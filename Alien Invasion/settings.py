57a86429d9fe4787407c5b60830ebebfedd499c1
medfusian <vlad.shumihai@gmail.com>
2023-02-12 15:00:22 +0300

    Adding a game project "Alien Invasion"

---
+++ Alien Invasion/settings.py
@@ -0,0 +1,19 @@
+class Settings():
+    """Класс для хранения всех настроек игры"""
+
+    def __init__(self):
+        """Инициализирует настройки игры"""
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (200, 200, 200)
+
+        # Настройки корабля
+        self.ship_speed_factor = 1.5
+
+        # Параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
